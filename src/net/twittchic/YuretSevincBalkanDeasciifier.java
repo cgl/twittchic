983bbfbe67a3fa168866201425c19bacfaad48f9
Çağıl Uluşahin <cagil.ulusahin@sahibinden.com>
2013-06-25 20:11:54 +0300

    deasciifier on work

--- src/net/twittchic/YuretSevincBalkanDeasciifier.java
+++ src/net/twittchic/YuretSevincBalkanDeasciifier.java
@@ -2,6 +2,9 @@ package net.twittchic;
 
 import turkish.Deasciifier;
 
+import java.io.*;
+import java.util.Scanner;
+
 /**
  * Created with IntelliJ IDEA.
  * User: cagilulusahin
@@ -10,10 +13,62 @@ import turkish.Deasciifier;
  * To change this template use File | Settings | File Templates.
  */
 public class YuretSevincBalkanDeasciifier {
-    public YuretSevincBalkanDeasciifier() {
-        Deasciifier d = new Deasciifier();
-
-        d.setAsciiString("Hadi bir masal uyduralim, icinde mutlu, doygun, telassiz durdugumuz.");
-        System.out.println(d.convertToTurkish());
+    public YuretSevincBalkanDeasciifier(String fileName) {
+        fFileName = fileName;
+        try {
+            process();
+        } catch (IOException e) {
+            e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+        }
     }
+
+    void process() throws IOException {
+        StringBuilder text = new StringBuilder();
+        String NL = System.getProperty("line.separator");
+        Scanner scanner = new Scanner(new FileInputStream(fFileName), fEncoding);
+        Writer out = new OutputStreamWriter(new FileOutputStream(outFileName), fEncoding);
+        Deasciifier d = new Deasciifier();
+        try {
+            while (scanner.hasNextLine()){
+                String s = scanner.nextLine();
+                d.setAsciiString(s);
+                text.append(s + NL);
+                text.append(d.convertToTurkish() + NL);
+            }
+            out.write(String.valueOf(text));
+        }
+        finally{
+            scanner.close();
+
+            out.close();
+        }
+    }
+
+    // PRIVATE
+    private final String fFileName;
+    private final String outFileName = "resources/output/deasciified.txt";
+    private final String fEncoding = "utf-8";
+    private final String INPUT_TEXT = "";
+    private String FIXED_TEXT = "But soft! what code in yonder program breaks?";
+
+    private void log(String aMessage){
+        System.out.println(aMessage);
+    }
+
+    public static void main(String[] args) {
+        String l;
+        /*
+        if (args.length < 1){
+
+            Scanner v = new Scanner(System.in);
+            l = v.next();
+        }*/
+
+        l = "resources/input/tokenized.txt";
+        if (args.length > 1)
+            l = args[1];
+        YuretSevincBalkanDeasciifier deasc = new YuretSevincBalkanDeasciifier(l);
+
+    }
+
 }
