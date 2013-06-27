b9a906cc2e2b7e1e359696ae3ebf02ecdd80f070
Çağıl Uluşahin <cagil.ulusahin@sahibinden.com>
2013-06-27 14:58:33 +0300

    changes on parser and tokenizer to improve accuracy

--- src/net/twittchic/Parser.java
+++ src/net/twittchic/Parser.java
@@ -1,5 +1,6 @@
 package net.twittchic;
 
+import com.sun.deploy.util.StringUtils;
 import net.zemberek.erisim.Zemberek;
 import net.zemberek.tr.yapi.TurkiyeTurkcesi;
 import turkish.Deasciifier;
@@ -16,10 +17,13 @@ import java.util.*;
  */
 public class Parser {
     static TreeMap<String, String> oovlist = new TreeMap<String,String>();
+    static TreeMap<String, String> ivlist = new TreeMap<String,String>();
     // PRIVATE
     private final String fFileName;
     private final String outFileName = "resources/output/parsed.txt";
     private final String needToNorm = "resources/output/parsed_oov_words.txt";
+    private final String normed = "resources/output/parsed_iv_words.txt";
+
     private final String fEncoding = "utf-8";
     private final String INPUT_TEXT = "";
     private String FIXED_TEXT = "But soft! what code in yonder program breaks?";
@@ -28,13 +32,17 @@ public class Parser {
     public Parser(String fileName) {
         tweets = new ArrayList<Tweet>();
         fFileName = fileName;
-        try {
-            process();
-        } catch (IOException e) {
-            e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
-        }
-        write();
+    }
 
+    public String ovv_statistics(){
+        int sum = 0;
+        int iv = 0;
+        for (Tweet tweet : this.tweets){
+            sum += tweet.getOovs().size();
+            iv  += tweet.getIvs().size();
+            //log(" " + (tweet.getOovs().size()));
+        }
+        return " Unique Ovv size " + oovlist.size() + " Unique Iv size " + ivlist.size() + " Ovvs " + sum + " Ivs " + iv;
     }
     public void write(){
         String NL = System.getProperty("line.separator");
@@ -53,44 +61,60 @@ public class Parser {
 
     }
 
-    void process() throws IOException {
+    void process(int limit) throws IOException {
         Scanner scanner = new Scanner(new FileInputStream(fFileName), fEncoding);
         Writer out2 = new OutputStreamWriter(new FileOutputStream(needToNorm), fEncoding);
         Zemberek z = new Zemberek(new TurkiyeTurkcesi());
         int i = 0;
         Tweet tweet;
         List<Tweet> tweets;
+        int sum = 0;
+
         try {
             while (scanner.hasNextLine()){
-                String s = scanner.nextLine();
+                String s = scanner.nextLine().trim();
                 tweet = new Tweet(s);
                 this.tweets.add(tweet);
                 i = 0;
                 for (String token : s.split(" ")) {
                     i++;
+                    if(token.length() == 0)
+                        continue;
                     if(isNumeric(token)|
-                            token.startsWith("http") | token.matches("(\\p{Punct})+|(\\.)+")){
+                        token.startsWith("http") | token.matches("(\\p{Punct})+|(\\.)+")){
                         continue;
                     }
-                    if(token.startsWith("#"))  {
-                        tweet.addHashtag(token,i);
+                    else if(isNumeric(token.substring(0,1))){
+                        tweet.addOov(token, i);
+                        oovlist.put(token.toLowerCase(new Locale("utf-8")), "");
+                    }
+                    else if(token.startsWith("#"))  {
+                        tweet.addHashtag(token, i);
                     }
                     else if(token.startsWith("@"))  {
-                        tweet.addMention(token,i);
+                        tweet.addMention(token, i);
                     }
-                    else if(!z.kelimeDenetle(token)) {
-                        tweet.addOov(token,i);
-                        oovlist.put(token, "-");
+                    else if(z.kelimeDenetle(token)) {
+                        ivlist.put(token.toLowerCase(Locale.forLanguageTag("tr-TR")), "");
+
+
+                        tweet.addIv(token, i);
+                        //ivlist.put(token.toLowerCase(new Locale("utf-8")),"-" );
                     }
                     else{
-                        System.out.println(token);
-                        tweet.addIv(token, i);
+
+                        tweet.addOov(token,i);
+                        oovlist.put(token.toLowerCase(Locale.forLanguageTag("tr-TR")), "");
                     }
+
                 }
                 ;
+                if(oovlist.size() > limit)
+                    break;
 
             }
             save(oovlist,needToNorm);
+            save(ivlist,normed);
         }
         finally{
             scanner.close();
@@ -117,8 +141,12 @@ public class Parser {
     private static void save(TreeMap<String,String> dictionary, String wordlist) throws IOException {
         FileWriter file = new FileWriter(wordlist);
         BufferedWriter writer = new BufferedWriter(file);
+
         for (Map.Entry<String, String> entry : dictionary.entrySet()) {
-            writer.append(entry.getKey() + " " + entry.getValue());    //TO DO space required in between
+            if(entry.getValue().isEmpty())
+                writer.append(entry.getKey());
+            else
+                writer.append(entry.getKey() + ";" + entry.getValue());    //TO DO space required in between
             writer.newLine();
         }
 
@@ -139,6 +167,16 @@ public class Parser {
         if (args.length > 1)
             l = args[1];
         Parser deasc = new Parser(l);
+        for (int limit : new int[]{500,700, 1000, 1500, 2000}){
+            try {
+                deasc.process(limit);
+            } catch (IOException e) {
+                e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+            }
+            deasc.log("Tweets: "+ deasc.tweets.size() + deasc.ovv_statistics());
+            //ovv_statistics();
+            deasc.write();
+        }
 
     }
 
