398cc0e72fc91645e503af08c0f181badffa513a
yener <yenercoder@gmail.com>
2013-07-01 17:52:48 +0200

    trainingresult eklenildi

--- src/net/twittchic/constants/Constants.java
+++ src/net/twittchic/constants/Constants.java
@@ -7,6 +7,7 @@ public class Constants {
     public static String needToNorm = "resources/output/parsed_oov_words.txt";
     public static String normed = "resources/output/parsed_iv_words.txt";
     public static String vocabulary = "resources/input/vocab";
+    public static String trainingFileName = "resources/output/trainresult.txt";
 
     public static String NL = System.getProperty("line.separator");
     public static final String fEncoding = "utf-8";
