a9e78a04bc8b6f6fcb44b81119f1e2ecc95666b0
yener <yenercoder@gmail.com>
2013-07-02 15:55:58 +0200

    some more testcases

--- src/net/twittchic/Baseline.java
+++ src/net/twittchic/Baseline.java
@@ -58,8 +58,10 @@ public class Baseline {
         write(b2,Constants.b2);
         List<Tweet> b3 = zemberekrandom(tweets);
         //write(b3,Constants.b3);
+        control.process(b3);
         List<Tweet> b4 = soundLevDict.process(tweets);
-        //write(b4,Constants.b4);
+        //write(b4,Constants.b4)
+        control.process(b4);
 
     }
 
