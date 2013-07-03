31a68471efd099f784b18a5d84a19e61332b780d
cemaivaz <cemrifkiaydin@gmail.com>
2013-07-03 11:58:48 +0300

    soundLevDict dosyasi updated edildi

--- src/net/twittchic/Baseline.java
+++ src/net/twittchic/Baseline.java
@@ -2,10 +2,7 @@ package net.twittchic;
 
 import net.twittchic.constants.Constants;
 
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.OutputStreamWriter;
-import java.io.Writer;
+import java.io.*;
 import java.util.List;
 
 import static net.twittchic.Recommendations.zemberekDegreeOne;
@@ -21,47 +18,54 @@ import static net.twittchic.Recommendations.zemberekrandom;
 public class Baseline {
     public static void main(String[] args) {
         String l;
-        /*
-        if (args.length < 1){
+
+        /*if (args.length < 1){
 
             Scanner v = new Scanner(System.in);
             l = v.next();
         }*/
         if (args.length > 1)
             l = args[1];
-        Parser parser = new Parser();
 
-        try {
-            parser.process(999999999);
-        } catch (IOException e) {
-            e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
-        }
-        //deasc.write();
-        List<Tweet> tweets = parser.getTweets();
+        List<Tweet> tweets = deserializeTweets();
         Deasciifier d = new Deasciifier(tweets);
         d.process();
-
-        //parser.setTweets(tweets);
-        //
-        // write(soundLevDict.process(tweets));
-        //write(tweets);
+        List<Tweet> b2 = zemberekDegreeOne(tweets);
+        List<Tweet> b3 = zemberekrandom(b2);
         Control control = new Control();
         control.process(tweets);
 
-        write(tweets,Constants.b1);
-        List<Tweet> b2 = zemberekDegreeOne(tweets);
-        control.process(b2);
-        write(b2,Constants.b2);
-        List<Tweet> b3 = zemberekrandom(tweets);
-        //write(b3,Constants.b3);
-        control.process(b3);
+        System.out.println("Cem asagidaki 2 satiri ekledi");
         List<Tweet> b4 = soundLevDict.process(tweets);
-        //write(b4,Constants.b4)
         control.process(b4);
 
+        //control.processB1B2B3(tweets);
+        //control.processB1B2B3(b3);
+        System.out.println("------------------------------------------------");
     }
 
-
+    public static List<Tweet> deserializeTweets()
+    {
+        List<Tweet> tweets = null;
+        try{
+            InputStream file = new FileInputStream( Constants.tweetsFile);
+            InputStream buffer = new BufferedInputStream( file );
+            ObjectInput input = new ObjectInputStream ( buffer );
+            try{
+                tweets = (List<Tweet>)input.readObject();
+            }
+            finally{
+                input.close();
+            }
+        }
+        catch(ClassNotFoundException ex){
+            System.out.println("DESerialize edilirken hata gerçekleşti 1 !!! : "+ex.getMessage());
+        }
+        catch(IOException ex){
+            System.out.println("DESerialize edilirken hata gerçekleşti 1 !!! : "+ex.getMessage());
+        }
+        return  tweets;
+    }
     public static void write(List<Tweet> tweets,String filename){
         try {
             Writer out = new OutputStreamWriter(new FileOutputStream(filename), Constants.fEncoding);
