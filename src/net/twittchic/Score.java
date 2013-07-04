ab54f28b3be89e8636a0342efcf7ed7c14de96ab
Çağıl Uluşahin <cagilulusahin@gmail.com>
2013-07-04 22:53:35 +0300

    score class added

---
+++ src/net/twittchic/Score.java
@@ -0,0 +1,80 @@
+package net.twittchic;
+
+import net.twittchic.constants.Constants;
+
+import java.util.*;
+
+import static net.twittchic.Baseline.deserializeTweets;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: cagil
+ * Date: 7/4/13
+ * Time: 10:20 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class Score {
+    public Score(){
+        create(1,1,1);
+    }
+
+    public static void create(double a,double b, double c){
+        List<Tweet> tweets = deserializeTweets(Constants.tweetsFile);
+        feedConfusionSetL(tweets);
+        feedConfusionSetG(tweets);
+        feedConfusionSetLM(tweets);
+        identifyIllFormedWords(tweets);
+        ArrayList<Double> scores;
+        int i;
+        for (Tweet tweet : tweets) {
+            final TreeMap<Integer,HashSet<String>> confusionSet = tweet.getConfusionSet();
+            HashMap<Integer,String> results = tweet.getResults();
+            for (Integer ind : confusionSet.keySet()) {
+                final HashSet<String> confWords = confusionSet.get(ind);
+                scores = new ArrayList<Double>(confWords.size());
+                for (String confWord : confWords) {
+                    scores.add(a * scoreL(ind, confWord, tweet) +
+                            b * scoreG(ind, confWord, tweet) +
+                            c * scoreLM(ind, confWord, tweet));
+                }
+                Double max = Collections.max(scores);
+                int chosenInd = scores.indexOf(max);
+                results.put(ind, (String) confWords.toArray()[chosenInd]);
+            }
+
+        }
+
+    }
+
+    private static Integer keyOfMax(HashMap<Integer, Double> scores) {
+        return null;  //To change body of created methods use File | Settings | File Templates.
+    }
+
+    private static double scoreLM(Integer ind, String confWord, Tweet tweet) {
+        return 0;  //To change body of created methods use File | Settings | File Templates.
+    }
+
+    private static double scoreG(Integer ind, String confWord, Tweet tweet) {
+        return 0;  //To change body of created methods use File | Settings | File Templates.
+    }
+
+    private static double scoreL(Integer ind, String confWord, Tweet tweet) {
+        return 0;  //To change body of created methods use File | Settings | File Templates.
+    }
+
+    private static void identifyIllFormedWords(List<Tweet> tweets) {
+        //To change body of created methods use File | Settings | File Templates.
+    }
+
+    private static void feedConfusionSetL(List<Tweet> tweets) {
+        //To change body of created methods use File | Settings | File Templates.
+    }
+
+    private static void feedConfusionSetG(List<Tweet> tweets) {
+        //To change body of created methods use File | Settings | File Templates.
+    }
+
+    private static void feedConfusionSetLM(List<Tweet> tweets) {
+        //To change body of created methods use File | Settings | File Templates.
+    }
+}
