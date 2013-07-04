0c4053719e5be5ab04e43b3bebdef086cb463fd3
cemaivaz <cemrifkiaydin@gmail.com>
2013-07-04 17:41:31 +0300

    soundex.java degistirildi

--- src/net/twittchic/soundex.java
+++ src/net/twittchic/soundex.java
@@ -31,11 +31,7 @@ public class soundex {
 			else if(lowWord.charAt(i) == 'ü')
 				ch = 'u';
 			if(!vowEl.contains(Character.toString(ch)))
-			{
 				temp1 += Character.toString(ch);
-			}
-			else if(vowEl.contains(Character.toString(ch)))
-				temp1 += "0";
 			
 		}
 		
@@ -49,7 +45,6 @@ public class soundex {
 				temp2 += Character.toString(temp1.charAt(i));
 			
 		}
-		temp2 = temp2.replaceAll("0", "");
 
 		return temp2;
 	}
